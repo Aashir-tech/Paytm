87eec33abd8d97feb8b42cde8d64f8adbf70cb66
Aashir-tech <aashirharis6@gmail.com>
2025-06-13 14:38:22 +0530

    Added home route to backend

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,35 @@
 const express = require("express");
-const cors = require("cors")
-const app = express()
-const {connectDB} = require('./db.js')
+const cors = require("cors");
+const app = express();
+const { connectDB } = require("./db.js");
 
-require('dotenv').config()
+require("dotenv").config();
 
 connectDB();
 
 //Always keep it abpve your main router
-app.use(cors({
-    origin : 'https://walletgo.vercel.app',
-    credentials : true,
-    methods : 'GET,HEAD,POST,PUT,PATCH,DELETE'
-}))
+app.use(
+  cors({
+    origin: "https://walletgo.vercel.app",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  })
+);
+
+app.get("/" , (req , res) => {
+    return res.json({
+        message : "Server is working in good condition"
+    })
+})
 
 app.use(express.json());
 
-const mainRouter = require('./routes/index.js')
+const mainRouter = require("./routes/index.js");
 
-app.use("/api/v1" , mainRouter);
+app.use("/api/v1", mainRouter);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT , () => {
-    console.log(`Server running on ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
